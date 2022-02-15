33a9293e8c029a04760b0cdcc385edd3ac7a68c5
Davi <davi.eugenio@hotmail.com>
2022-02-15 13:21:38 -0300

    exerc-resolvido-arrays

---
+++ modulo1/strings-arrays/index.js
@@ -0,0 +1,44 @@
+
+// let array
+// console.log('a. ', array)
+
+// array = null
+
+// // resposta no console = a.
+
+// console.log('b. ', array)
+
+// // resposta no console = .b
+
+
+// array = [3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+// console.log('c. ', array.length)
+
+// resposta = c. 10
+
+
+// let i = 0
+// console.log('d. ', array[i])
+
+// resposta = d. 3
+
+
+// array[i+1] = 19
+// console.log('e. ', array)
+
+// e. [3, 19, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+
+// const valor = array[i+6]
+// console.log('f. ', valor)
+
+// resposta = f. 9
+
+/* 2.
+
+const frase = prompt ("Digite uma frase")
+console.log(frase.toUpperCase().replaceAll("A", "I"), frase.length)
+
+"Subi num ônibus em Marrocos"
+
+Resposta - SUBI NUM ÔNIBUS EM MIRROCOS 26
+
