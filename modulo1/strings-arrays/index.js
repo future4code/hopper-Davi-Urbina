16ba3b8f9f6ef834ce18f482e145d3b07fab6a23
Davi <davi.eugenio@hotmail.com>
2022-02-15 15:15:59 -0300

    exe-array-entrega

--- modulo1/strings-arrays/index.js
+++ modulo1/strings-arrays/index.js
@@ -40,5 +40,59 @@ console.log(frase.toUpperCase().replaceAll("A", "I"), frase.length)
 
 "Subi num ônibus em Marrocos"
 
-Resposta - SUBI NUM ÔNIBUS EM MIRROCOS 26
+Resposta - SUBI NUM ÔNIBUS EM MIRROCOS 26*/
+
+/* exercício escrita de código
+
+1 .
+    let nomeUsuario = prompt("Digite o seu nome")
+    let emailUsuario = prompt("Digite seu e-mail")
+
+    console.log(`O e-mail ${emailUsuario} foi cadastrado com sucesso. Seja bem-vinda(o), ${nomeUsuario}!`) */
+
+// 2
+
+// a
+
+// let comidasPreferidas = ["lasanha", "nhoc", "sadala de frutas", "açai", "pizza"]
+
+// console.log(comidasPreferidas)
+
+// // b
+
+// console.log("Essas são as minhas comidas preferidas:")
+// console.log(comidasPreferidas[0])
+// console.log(comidasPreferidas[1])
+// console.log(comidasPreferidas[2])
+// console.log(comidasPreferidas[3])
+// console.log(comidasPreferidas[4])
+
+// // //c
+
+// let comidaPreferidaUsuario = prompt("Qual sua comida preferida")
+
+// comidasPreferidas[1] = comidaPreferidaUsuario
+// console.log(comidasPreferidas)
+
+
+//3
+
+// let listaDeTarefas = []
+// let tarefa1 = prompt("Digite a primeira tarefa")
+// let tarefa2 = prompt("Digite a segunda tarefa")
+// let tarefa3 = prompt("Digite a terceira tarefa")
+
+// listaDeTarefas.push(tarefa1, tarefa2, tarefa3)
+
+// console.log(listaDeTarefas)
+
+// let tarefaRealizada = prompt("Qual tarefa já foi realizada, 1,2 ou 3?")
+// tarefaRealizada = (tarefaRealizada - 1)
+// listaDeTarefas.splice(tarefaRealizada, 1)
+// console.log(listaDeTarefas)
+
+
+
+
+
 
